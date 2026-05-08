e8497754089d5d65b70b45d0f19ac47117e32a6b
faridisa <faridisa@code.edu.az>
2026-05-08 19:52:27 +0400

    Feat

--- BioLife/obj/Debug/net10.0/BioLife.AssemblyInfo.cs
+++ BioLife/obj/Debug/net10.0/BioLife.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BioLife")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d153da6486562d57a878262eef213770c450041")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33afdaf34efb9e786f877cd576b5c99d6c92ea65")]
 [assembly: System.Reflection.AssemblyProductAttribute("BioLife")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BioLife")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
